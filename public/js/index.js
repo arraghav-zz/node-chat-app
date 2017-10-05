a21fa0b9b5042488e70dfc3761d0a3f789256421
arraghav <arraghav@hotmail.com>
2017-10-05 16:05:07 +0530

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    to: 'Receiver',
-    text: 'Hello, creating new message'
-  });
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -15,14 +15,13 @@ app.use (express.static(publicPath));
 io.on('connection', (socket) => {
   console.log ('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Sender Name',
-    text: 'Hello, testing new Message',
-    createAt: 1234
-  });
-
   socket.on('createMessage', (newMessage) => {
     console.log('createMessage', newMessage);
+    io.emit('newMessage', {
+      from: newMessage.from,
+      text: newMessage.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
